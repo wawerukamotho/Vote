8f9513d70ccab527f6146dfc815a8c8180d2e743
wawerukamotho <wawerukamotho82@gmail.com>
2026-05-16 22:18:42 +0300

    initial setup

---
+++ programs/votingapp/src/lib.rs
@@ -0,0 +1,88 @@
+use anchor_lang::prelude::*;
+
+declare_id!("BGKwvkV3e9DEr38NxKXtd1UTuHoGPwrF6yMDSoDRSGvc");
+
+#[program]
+pub mod votingapp {
+    use super::*;
+
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
+        let  poll = & mut ctx.accounts.poll_account;
+        poll.poll_name = name;
+        poll.description = description;
+        poll.poll_start_time = start;
+        poll.poll_end_time = end;
+        Ok(())
+
+
+        
+
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitPoll <'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+
+pub struct InitCandidate <'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + CandidateAccount::INIT_SPACE,
+        seeds = [b"candidate".as_ref(), poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+    pub system_program: Program<'info, System>,
+    
+}
+
+
+
+#[account]
+#[derive(InitSpace)]
+
+pub struct PollAccount{
+    #[max_len(32)]
+    pub poll_name: String,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start_time: u64,
+    pub poll_end_time: u64,
+    pub poll_option_index: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
